97ce39aec821a4e78dc78739a76054faa5ed6d17
sergio <sergio@secondary.home>
2019-08-23 16:27:40 -0300

    Login ok.

--- homebanking/web/js/programa.js
+++ homebanking/web/js/programa.js
@@ -1,7 +1,29 @@
 class Login {
     
+    static baseDeDatos(){
+        // Esta seria nuestra base de datos.
+        // En esta primer etapa lo manejamos desde el cliente, luego ira en el servidor con SQL.
+        let cuentas = {
+            user : "pedro",
+            pass : "123"
+        };
+        return cuentas;
+    }
+    
     static entrar(){
+        let user = document.querySelector("#user").value;
+        let pass = document.querySelector("#pass").value;
+        let datoCuenta = Login.baseDeDatos();
+        console.log("Base de datos: " + datoCuenta.user);
         
+        if (datoCuenta.user === user & datoCuenta.pass === pass) {
+            console.log("ENTRASTE");
+            Login.showPnlCuenta();
+            Login.hidePnlLogin();
+        } else {
+            console.log("A la calle!");
+            
+        }
     }
     
     static salir() {
@@ -11,10 +33,11 @@ class Login {
     static init() {
         Login.showPnlLogin();
         Login.hidePnlCuenta();
+        document.querySelector("#loginBtn").setAttribute("onclick","Login.entrar();");
     }
     
     static showPnlCuenta() {
-        document.querySelector("#cuentaPnl").style.display = "block";
+        document.querySelector("#cuentaPnl").style.display = "flex";
     }
     
     static showPnlLogin() {
